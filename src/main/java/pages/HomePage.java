4936b1fbc60993b6ed9df511f1400464cf573d3c
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2021-10-28 17:03:26 +0300

    add file conf.properties, class ConfProperties

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.By;
 import java.awt.*;
 
 public class HomePage {
-    //public List logoHolder = driver.findElements(By.xpath(//div[@id='global_header']//span[@id='logo_holder']//a));
-    //public List buttonAbout = driver.findElements(By.xpath(//div[@id='global_header']//div[@class='supernav_container']//a[@class='menuitem']));
-    //public List submenuStore = driver.findElements(By.xpath(div[@id='global_header']//div[@class='supernav_container']//a[@class='menuitem supernav'][@data-tooltip-content='.submenu_store']));
+   /*
+    public List logoHolder = driver.findElements(By.xpath(ConfProperties.getProperty("XPathLogoHolder")));
+    public List buttonAbout = driver.findElements(By.xpath(ConfProperties.getProperty("XPathButtonAbout")));
+    public List gamersOnline = driver.findElements(By.xpath(ConfProperties.getProperty("XPathGamersOnline")));
+    public List gamersInGame = driver.findElements(By.xpath(ConfProperties.getProperty("XPathGamersInGame")));
+    public List submenuStore = driver.findElements(By.xpath(ConfProperties.getProperty("XPathSubmenuStore")));
+*/
 }
